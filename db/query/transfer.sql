21b8eb4e4ba2e300c1ace3b2f6d9ab302555e9a9
fayob <adedejifavour6@gmail.com>
2023-05-29 21:46:57 +0100

    Add transfer query

---
+++ db/query/transfer.sql
@@ -0,0 +1,7 @@
+-- name: CreateTransfer :one
+INSERT INTO transfers(from_account_id, to_account_id, amount) 
+VALUES ($1, $2, $3) RETURNING *;
+
+-- name: GetTransfer :one
+SELECT * FROM transfers
+WHERE id = $1 LIMIT 1;
