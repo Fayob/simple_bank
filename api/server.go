8abe8c0b3a4a30ec394b81ddb971afa9393c4f78
fayob <adedejifavour6@gmail.com>
2023-05-31 11:40:49 +0100

    Add api server functionality

---
+++ api/server.go
@@ -0,0 +1,71 @@
+package api
+
+import (
+	"fmt"
+	db "simple_bank/db/sqlc"
+	"simple_bank/token"
+	"simple_bank/util"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
+)
+
+// Server serves HTTP requests for our banking service
+type Server struct {
+	config util.Config
+	store  db.Store
+	tokenMaker token.Maker
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and setup routing
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config: config,
+		store: store,
+		tokenMaker: tokenMaker,
+	}
+	
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
+	server.setupRouter()
+
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
+
+	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
+
+	authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker))
+
+	authRoutes.POST("/accounts", server.createAccount)
+	authRoutes.GET("/accounts/:id", server.getAccount)
+	authRoutes.GET("/accounts", server.listAccount)
+	authRoutes.PATCH("/accounts/:id", server.updateAccount)
+	authRoutes.DELETE("/accounts/:id", server.deleteAccount)
+
+	authRoutes.POST("/transfers", server.createTransfer)
+
+	server.router = router
+}
+
+// Start runs the HTTP server on a specific address
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
