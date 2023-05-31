24ad81aa09cedf0ecae308982d7b65ca51014fea
fayob <adedejifavour6@gmail.com>
2023-05-31 11:38:07 +0100

    Add main.go

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"simple_bank/api"
+	db "simple_bank/db/sqlc"
+	"simple_bank/util"
+
+	_ "github.com/lib/pq"
+)
+
+func main()  {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server, err := api.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server")
+	}
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+}
