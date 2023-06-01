bd7b32fae44e7137be3481c3c3bbcf3a474bbee9
fayob <adedejifavour6@gmail.com>
2023-06-01 10:41:23 +0100

    Add user api functionality

---
+++ api/validator.go
@@ -0,0 +1,14 @@
+package api
+
+import (
+	"simple_bank/util"
+
+	"github.com/go-playground/validator/v10"
+)
+
+var validCurrency validator.Func = func (fieldLevel validator.FieldLevel) bool {
+	if currency, ok := fieldLevel.Field().Interface().(string); ok {
+		return util.IsSupportedCurrency(currency)
+	}
+	return false
+}
